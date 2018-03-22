ea9c210aafbfd43630f65f1df6c4b20447dc9c14
Purabsh93 <purab.shah@airpay.co.in>
2018-03-22 11:28:10 +0530

    add translations

--- js/main.js
+++ js/main.js
@@ -23,7 +23,6 @@ function loadTranslateJson(){
     if(languageJson && typeof languageJson === "string"){
         languageJson = JSON.parse(languageJson);
     }
-
     if(!languageJson){
         $.ajax('./language/'+jsonFileName,{
             success: $.proxy(function(currentLang,data){
